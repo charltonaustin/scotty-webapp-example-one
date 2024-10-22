dfe1144d99569f0f9ec7f6e72a6e11f533386c68
charltonaustin <charlton.austin@gmail.com>
2024-10-22 05:18:12 -0700

    Following along https://dev.to/parambirs/how-to-write-a-haskell-web-servicefrom-scratch---part-3-5en6 part 1

--- app/Main.hs
+++ app/Main.hs
@@ -8,3 +8,11 @@ main = scotty 3000 $ do
     html "Hello World!"
   get "/func" $ do
     html hello
+  get "/new" $ do                         -- handle GET request on "/" URL
+    text "This was a GET request!"     -- send 'text/plain' response
+  delete "/new" $ do
+    html "This was a DELETE request!"  -- send 'text/html' response
+  post "/new" $ do
+    text "This was a POST request!"
+  put "/new" $ do
+    text "This was a PUT request!"
