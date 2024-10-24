87997d322ee266c99552dbff2d029c56600508f3
charltonaustin <charlton.austin@gmail.com>
2024-10-24 06:50:51 -0700

    Adding in some updates and playing with my ide.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Web.Scotty
+    ( captureParam,
+      delete,
+      get,
+      html,
+      matchAny,
+      notFound,
+      queryParam,
+      post,
+      put,
+      scotty,
+      setHeader,
+      status,
+      text )
 import Lib (hello)
+import Network.HTTP.Types
 
 
 main = scotty 3000 $ do
@@ -16,3 +30,22 @@ main = scotty 3000 $ do
     text "This was a POST request!"
   put "/new" $ do
     text "This was a PUT request!"
+  -- set a header:
+  post "/set-headers" $ do
+   status status302  -- Respond with HTTP 302 status code
+   setHeader "Location" "http://www.google.com.au"
+  -- named parameters:
+  get "/askfor/:word" $ do
+    w <- captureParam "word"
+    html $ mconcat ["<h1>You asked for ", w, ", you got it!</h1>" ]
+  -- unnamed parameters from a query string or a form:
+  post "/submit" $ do  -- e.g. http://server.com/submit?name=somename
+   name <- queryParam "name"
+   text name
+  -- match a route regardless of the method
+  matchAny "/all" $ do
+   text "matches all methods"
+  -- handler for when there is no matched route
+  -- (this should be the last handler because it matches all routes)
+  notFound $ do
+   text "there is no such route."
